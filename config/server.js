cc76a6608477c41cf3d8e3d815e115fad8ccde7a
Russell James <jamesru@oregonstate.edu>
2020-12-19 20:31:11 -0900

    first. movie and category added

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f6407fe46febf345f72369fa15f78c75'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '46d8badd-043f-48f8-ac8e-a436181acae9'
+};
